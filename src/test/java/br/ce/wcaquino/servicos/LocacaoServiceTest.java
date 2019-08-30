99cf2fa2b96c22dfa3065f0df85dc22d75d9cf85
Daniel Paulino <daniel-paulino@hotmail.com>
2019-08-29 21:35:51 -0300

    Tratamento de Exceções, três formas de tratar as exceções

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -3,6 +3,7 @@ package br.ce.wcaquino.servicos;
 import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.Date;
 
@@ -11,6 +12,7 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -18,30 +20,69 @@ import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
-	
+
 	@Rule
-	public ErrorCollector error= new ErrorCollector();
-	
+	public ErrorCollector error = new ErrorCollector();
+
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
 	@Test
 	public void teste() {
-		//cenario
+		// cenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 0, 4.0);
-		
-		//acao
+		Filme filme = new Filme("Filme 1", 1, 4.0);
+
+		// acao
 		Locacao locacao;
 		try {
 			locacao = service.alugarFilme(usuario, filme);
-			// Destaca os errors com error(O esperado dos dois parâmetros é definido no ultimo param que é is())
+			// Destaca os errors com error(O esperado dos dois parâmetros é definido no
+			// ultimo param que é is())
 			error.checkThat(locacao.getValor(), is(equalTo(4.0)));
-			error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()),is(true));
+			error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 			error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			Assert.fail();
+			Assert.fail("Lançado a excessão");
 		}
+
+	}
+
+	// Três formas de verificar com exceção
+	@Test(expected = Exception.class)
+	public void testeFilmeSemEstoque() throws Exception {
+		LocacaoService service = new LocacaoService();
+		Usuario usuario2 = new Usuario("Usuario 1");
+		Filme filme2 = new Filme("Filme 1", 0, 4.0);
+		service.alugarFilme(usuario2, filme2);
+	}
+
+	@Test
+	public void testeFilmeSemEstoque2() {
+		LocacaoService service = new LocacaoService();
+		Usuario usuario2 = new Usuario("Usuario 1");
+		Filme filme2 = new Filme("Filme 1", 0, 4.0);
+
+		try {
+			service.alugarFilme(usuario2, filme2);
+		} catch (Exception e) {
+			assertThat(e.getMessage(), is("Filme sem estoque"));
+		}
+	}
+
+	@Test
+	public void testeFilmeSemEstoque3() throws Exception {
+		LocacaoService service = new LocacaoService();
+		Usuario usuario2 = new Usuario("Usuario 1");
+		Filme filme2 = new Filme("Filme 1", 0, 4.0);
 		
+		exception.expect(Exception.class);
+		exception.expectMessage("Filme sem estoque");
+		
+		service.alugarFilme(usuario2, filme2);		
+
 	}
 }
