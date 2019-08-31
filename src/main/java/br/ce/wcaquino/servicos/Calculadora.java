1fc6f6c3e49d194dca233590de170dfb0687ac0a
Daniel Paulino <daniel-paulino@hotmail.com>
2019-08-30 23:40:46 -0300

    TDD Desenvolvimento orientado a testes, parte 1 FINAL

--- src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -1,10 +1,24 @@
 package br.ce.wcaquino.servicos;
 
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
 public class Calculadora {
 
 	public int soma(int i, int j) {
 		// TODO Auto-generated method stub
-		return i+j;
+		return i + j;
+	}
+
+	public int subtrair(int i, int j) {
+		// TODO Auto-generated method stub
+		return i - j;
+	}
+
+	public int dividir(int i, int j) throws NaoPodeDividirPorZeroException {
+		if (j == 0) {
+			throw new NaoPodeDividirPorZeroException();
+		}
+		return i / j;
 	}
 
 }
--- src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -1,23 +1,70 @@
 package br.ce.wcaquino.servicos;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
 public class CalculadoraTest {
+	
+	private Calculadora calculadora;
+	
+	@Before
+	public void setup() {
+		calculadora = new Calculadora();
+	}
 
 	@Test
 	public void calculaSomaDoisNumeros() {
-		//Cenário
-		int i=5;
-		int j=3;
-		Calculadora calculadora= new Calculadora();
-		
-		//Ação
-		int resultado= calculadora.soma(i,j);
-		
-		
-		//Verificação
-		Assert.assertEquals(8,resultado);
+		// Cenário
+		int i = 5;
+		int j = 3;
+
+		// Ação
+		int resultado = calculadora.soma(i, j);
+
+		// Verificação
+		Assert.assertEquals(8, resultado);
 	}
-	
+
+	@Test
+	public void calculaSubtrairDoisNumeros() {
+		// Cenário
+		int i = 5;
+		int j = 3;
+
+		// Ação
+		int resultado = calculadora.subtrair(i, j);
+
+		// Verificação
+		Assert.assertEquals(2, resultado);
+	}
+
+	@Test
+	public void calculaDividirDoisNumeros() throws NaoPodeDividirPorZeroException {
+		// Cenário
+		int i = 6;
+		int j = 3;
+
+		// Ação
+		int resultado = calculadora.dividir(i, j);
+
+		// Verificação
+		Assert.assertEquals(2, resultado);
+	}
+
+	@Test(expected = NaoPodeDividirPorZeroException.class)
+	public void deveLançarExceptionDividirPorZero() throws NaoPodeDividirPorZeroException {
+		// Cenário
+		int i = 10;
+		int j = 0;
+
+		// Ação
+		int resultado = calculadora.dividir(i, j);
+
+		// Verificação
+		Assert.assertEquals(2, resultado);
+	}
+
 }
