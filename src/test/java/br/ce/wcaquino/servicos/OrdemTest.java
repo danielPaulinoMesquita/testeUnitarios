f4230dfef134b8e4583e9527107960be68c43db7
Daniel Paulino <daniel-paulino@hotmail.com>
2019-08-30 22:44:55 -0300

    Ordenando os testes, forma recomendada

--- src/test/java/br/ce/wcaquino/servicos/OrdemTest.java
+++ src/test/java/br/ce/wcaquino/servicos/OrdemTest.java
@@ -1,26 +1,31 @@
 package br.ce.wcaquino.servicos;
 
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+/*
+ * @FixMethodOrder(MethodSorters.NAME_ASCENDING) ordena os testes por ordem alfabetica
+ */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OrdemTest {
 	
 	static int i = 0;
 
+	@Test
 	public void inicia() {
 		i++;
 	}
 	
-	//Esse metodo depende do outro pra rodar de forma correta, 
+	// Esse metodo 'verifica' depende do outro pra rodar de forma correta, 
 	// ou seja ele deve ser o segundo a ser executado
+	
+	@Test
 	public void verifica() { 
 		Assert.assertEquals(1,i);
 	}
 	
-	@Test
-	public void testeGeral() {
-		inicia();
-		verifica();
-		
-	}
+	
 }
